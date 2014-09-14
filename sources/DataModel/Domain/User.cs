8d72502effdf4136fd875eda9a614d5e40ec81e4
Sergey Zwezdin <sergey@zwezdin.com>
2014-09-14 11:31:40 +0600

    Initial commit
    Moved from "https://github.com/sergun/CQS-Sample"

---
+++ sources/DataModel/Domain/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataModel.Domain
+{
+	[Table("Users")]
+	public class User
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public int Id { get; set; }
+
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public bool IsActive { get; set; }
+	}
+}
---
+++ sources/TestApp/IoC/Configuration.cs
@@ -0,0 +1,35 @@
+﻿using DataModel.Commands;
+using DataModel.Core;
+using DataModel.Queries;
+using EFDataAccess;
+using EFDataAccess.Commands;
+using EFDataAccess.Queries;
+using Ninject.Modules;
+using TestApp.Commands;
+
+namespace TestApp.IoC
+{
+	public class Configuration : NinjectModule
+	{
+		public override void Load()
+		{
+			// Infrastructure
+			Bind<IQueryFactory>().ToMethod(t => new QueryFactory(x => Container.Current.Resolve(x))).InTransientScope();
+			Bind<ICommandsFactory>()
+				.ToMethod(t => new CommandFactory(x => (object[]) Container.Current.ResolveAll(x)))
+				.InTransientScope();
+
+			// DbContext
+			Bind<ApplicationDbContext>().ToSelf().WithConstructorArgument("connectionString", "MyContext");
+
+			// Queries
+			Bind<IActiveUsersQuery>().To<ActiveUsersQuery>().InTransientScope();
+			Bind<IInactiveUsersQuery>().To<InactiveUsersQuery>().InTransientScope();
+			Bind<IUserByEmailQuery>().To<UserByEmailQuery>().InTransientScope();
+
+			// Commands
+			Bind<ICommandHandler<CreateUserCommand>>().To<CreateUserCommandHandler>().InTransientScope();
+			Bind<ICommandHandler<CreateUserCommand>>().To<CreateUserNotificationCommandHandler>().InTransientScope();
+		}
+	}
+}
