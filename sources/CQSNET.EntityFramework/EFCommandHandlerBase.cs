0ae8dbaa19fd1644586702ac8cef9092d97aa3cb
Sergey Zwezdin <sergey@zwezdin.com>
2014-09-14 15:26:01 +0600

    Entity Framework implementation added

---
+++ sources/CQSNET.EntityFramework/EFCommandHandlerBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace CQSNET.EntityFramework
+{
+	public abstract class EFCommandHandlerBase<TCommand, TContext> : ICommandHandler<TCommand>, IDisposable
+		where TCommand : ICommand
+		where TContext : DbContext
+	{
+		protected readonly TContext DbContext;
+
+
+		protected EFCommandHandlerBase(TContext dbContext)
+		{
+			DbContext = dbContext;
+		}
+
+
+		public abstract void Execute(TCommand command);
+
+
+		public void Dispose()
+		{
+			DbContext.Dispose();
+		}
+	}
+}
---
+++ sources/CQSNET.EntityFramework/EFQueryBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace CQSNET.EntityFramework
+{
+	public abstract class EFQueryBase<TContext> : IQuery
+		where TContext : DbContext
+	{
+		protected readonly TContext DbContext;
+
+		protected EFQueryBase(TContext dbContext)
+		{
+			DbContext = dbContext;
+		}
+	}
+}
